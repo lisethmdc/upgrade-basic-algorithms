150bee80a01ba232245c5b72f045643b69e5bb49
lisethmdc <lismc1992@gmail.com>
2021-11-23 20:52:16 +0100

    Iteration#3

---
+++ Operadores.js
@@ -0,0 +1,35 @@
+//1.1
+
+let mult = 5 * 10;
+
+alert("El resultado de la mutiplicación es: " + mult);
+
+
+//1.2
+
+let div = 10 / 2;
+
+alert("El resultado de la división es: " + div);
+
+
+//1.3
+
+let dec = 15 / 9;
+
+alert("El resultado con decimales es: " + dec);
+
+
+//1.4
+
+let y = 10;
+let z = 5;
+let x = y+=z;
+
+
+//1.5
+
+let y2 = 10;
+let z2 = 5;
+let x2 = y2*=z2;
+
+
