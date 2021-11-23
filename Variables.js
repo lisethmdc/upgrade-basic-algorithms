ea59061e78c2c9bf0768c944bd84d0406f44bd68
lisethmdc <lismc1992@gmail.com>
2021-11-23 20:20:11 +0100

    add comment

--- Variables.js
+++ Variables.js
@@ -1,8 +1,12 @@
-let myFavoriteHero = 'Hulk'
+//1.1
+let myFavoriteHero = 'Hulk';
 
-let x = 50
+//1.2
+let x = 50;
 
-let h = 5
-let y = 10
+//1.3
+let h = 5;
+let y = 10;
 
-let z = h+y
+//1.4
+let z = h+y;
