69826d71654bdd7a30e1c5f1897f0dee3ffe5eaf
lisethmdc <lismc1992@gmail.com>
2021-11-24 19:58:50 +0100

    Iteration#6

---
+++ Bucles.js
@@ -0,0 +1,25 @@
+/* //1.1
+
+for (var i = 0; i <= 9; i++) {
+    console.log(i);
+};
+
+
+//1.2
+
+for (var i = 0; i <= 9; i++) {
+   if (i % 2 === 0) {
+       console.log(i);
+   }
+};
+
+ */
+//1.3
+
+for (var i = 0; i <= 10; i++) {
+    if (i === 10) {
+        console.log('Dormido');
+    }else {
+    console.log('Intentando dormir');
+    }
+};
